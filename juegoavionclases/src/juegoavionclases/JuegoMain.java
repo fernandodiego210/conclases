e394dc5044ae222193ecf369b23d856ef5592de3
fernandodiego210 <f.d.s.h2023@gmail.com>
2025-06-30 13:16:52 -0500

    nombre

--- juegoavionclases/src/juegoavionclases/JuegoMain.java
+++ juegoavionclases/src/juegoavionclases/JuegoMain.java
@@ -7,11 +7,5 @@ public class JuegoMain {
         JFrame ventana = new JFrame("IRAN VS E.E.U.U.");
         PanelJuego juego = new PanelJuego();
         
-        ventana.add(juego);
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.setResizable(false);
-        ventana.pack();
-        ventana.setLocationRelativeTo(null);
-        ventana.setVisible(true);
-    }
+        }
 }
